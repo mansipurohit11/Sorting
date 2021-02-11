dc5a00bd57e300da683709ecd6d2a18a309ee473
mansipurohit11 <mansipurohit11@gmail.com>
2021-02-11 23:01:22 +0530

    MergeSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -29,7 +29,7 @@ public class BubbleSort {
                 System.out.println("Array After Bubble Sort");  
                 for(int i=0; i < arr.length; i++){  
                         System.out.print(arr[i] + " ");  
-                }  
+                }
    
         }  
 }
